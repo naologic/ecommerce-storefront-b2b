0335093395f2d9407da6bcd0353280d87e54ea94
Florin <galanflorind@gmail.com>
2021-09-23 13:55:50 +0300

    Connecting selected variants for landing page

--- apps/ecommerce-storefront-b2b/src/app/shared/pipes/check-image-fallback.pipe.ts
+++ apps/ecommerce-storefront-b2b/src/app/shared/pipes/check-image-fallback.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Variant } from "../../interfaces/product";
 
 @Pipe({
     name: 'checkImageFallback',
 })
 export class CheckImageFallbackPipe implements PipeTransform {
-    public transform(value: string): string {
+    public transform(value: string, selectedVariant?: Variant): string {
+        if (selectedVariant?.images?.length && selectedVariant?.images[0]) {
+            return selectedVariant.images[0];
+        }
         // -->Fallback: image
         const fallback = 'assets/images/image-not-available.png';
         // -->Check: image
